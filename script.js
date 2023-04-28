30f4acd3fff3f3d6d6633890226057eee87d6a77
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-04-27 23:31:09 -0400

    added the rest of the functions

--- script.js
+++ script.js
@@ -126,3 +126,86 @@ document.addEventListener("click", function (event) {
     }
 })
 
+var i = 0;
+function onclickHandler(event) {
+     
+    if(timer<=0){
+        clearInterval(timeCount);
+        divContEL.style.display="none";
+        displayResult();
+    }
+
+    var answerText = event.target.textContent 
+    if (answerText === questions[i].answer) {
+        timer = timer;
+        responsDiv.setAttribute("style", "color: green")
+        responsDiv.textContent = "Correct";
+    } else {
+        responsDiv.setAttribute("style", "color: red")
+        responsDiv.textContent = "Wrong";
+        timer = timer - 15;
+     }
+    
+     if (i < questions.length-1) {
+        i++;
+        setTimeout(function () {
+        displayQuestions();
+        responsDiv.textContent = "";
+    }, 1000)
+    }else {
+        setTimeout(function () {
+            responsDiv.textContent = "";
+            displayResult();
+            clearInterval(timeCount);
+        }, 500)
+        
+        divContEL.innerHTML = '';
+     }
+     
+// Function to display users final score 
+    function displayResult() {
+        finishDiv.style.visibility = "visible";
+        timeElement.textContent = "Time:" + " " + timer;
+        var HighScores = timer;
+        localStorage.getItem(HighScores)
+        finalScore.textContent = "Your finally score is: " + HighScores;
+         localStorage.setItem("HighScores", HighScores)
+        }
+    }
+// Function to show the last page  
+    function renderLastItem() {
+    var yourScore = localStorage.getItem("HighScores");
+     var yourInitial = localStorage.getItem("Initial");
+     if (yourScore && yourInitial === "") {
+        return
+    }
+    finishDiv.textContent = "";
+    var finaPageEl = document.querySelector(".final-page");
+    finaPageEl.style.visibility = "visible";
+    var initialAndScore = document.querySelector("#staticEmail");
+    initialAndScore.value = yourInitial + ":" + " " + yourScore;
+    }
+
+    document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var initialInput = document.querySelector("#inputInitial").value;
+    if (initialInput === "") {
+        errMsg.setAttribute("style", "color: red")
+        errMsg.textContent = "Initial input field cannot be empty"
+    } else {
+        errMsg.textContent = "";
+        localStorage.getItem(initialInput)
+        localStorage.setItem("Initial", initialInput)
+         renderLastItem()
+        }
+    })
+
+// This function will refresh the page and send user back to begining page when go back button is clicked 
+function init() {
+     location.reload();
+}
+
+// This function will  clear initial and score displayed on the final page
+function clearScore() {
+    initialAndScore.value = "";
+}
