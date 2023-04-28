4d248f070d74db5c6f597d08fcd4f67b37ac35b1
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-04-27 22:27:03 -0400

    added array of questions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// created variables
 var timeElement = document.querySelector("#time");
 var wrapperElement = document.querySelector(".wrapper");
 var btnElement = document.querySelector("#start");
@@ -13,3 +14,34 @@ var responsDiv = document.querySelector("#response");
 var finaPageEl = document.querySelector(".final-page");
 var initialAndScore = document.querySelector("#staticEmail");
 var firstPageEl = document.querySelector(".first-page");
+
+// Create an  array of questions
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "There are 3 different ways in which a JavaScript code can be involved in an HTML file. Selct the one that's not correct.",
+        choices: ["Inline", "Import", "External", "Internal"],
+        answer: "Import",
+    },
+    {
+        title: "How to create an array in js ?",
+        choices: ["var A[]=", "var A{}=", "var A=[]", "var A={}"],
+
+        answer: "var A=[]",
+    },
+    {   
+        title: "HTML element that can be accessed in a Javascript code: Chose the one that will return an array of elements",
+        choices: ["getElementById('idname')", "getElementsByClass('classname')", 
+        "getElementsByTagName('tagname')", "querySelectorAll()"],
+        answer: "querySelectorAll()",
+    }
+]
